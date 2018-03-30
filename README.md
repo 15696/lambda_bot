4e5648b98da06ed1e44c57380c021954340b2a44
mental32 <m3nta1@yahoo.com>
2018-03-31 02:13:02 +0300

    Update, added new commands

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # lambda_bot
 A discord bot written with pure lambdas using discord.py
+
+
+requirements:
+ - python3.5 -> 3.6
--- lambda_bot.py
+++ lambda_bot.py
@@ -1,6 +1,9 @@
 import discord
 import asyncio
 
+import signal
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+
 token = "TOKEN"
 
-(lambda run, *args: run(discord.Client(), *args))((lambda client, *args: [func(client) for func in args]), (lambda c: [setattr(c, key, value) for key, value in {'on_ready': asyncio.coroutine((lambda: print('Logged in as\n{0}\n{1}\n------'.format(c.user.name, c.user.id)))), 'on_message': asyncio.coroutine((lambda m: ({'ping': (lambda m: c.loop.create_task(m.channel.send('pong')))}[m.content[2:]](m) if m.content.startswith('!!') else c.on_message)))}.items()]), (lambda c: print(c.run(token))))
+(lambda run, *args: run(discord.Client(), *args))((lambda client, *args: [func(client) for func in args]),(lambda client: setattr(client, '__await', client.loop.create_task)),(lambda client: setattr(client, '__commands', {'ping': (lambda message: client.__await(message.channel.send('pong'))), 'whoami': (lambda message: client.__await(message.channel.send('You are %s' %(message.author)))), 'help': (lambda message: client.__await(message.channel.send('```\n{0}```'.format('\n'.join(name for name in client.__commands)))))})), (lambda c: [setattr(c, key, value) for key, value in {'on_ready': asyncio.coroutine((lambda: print('Logged in as\n%s\n%s\n------' %(c.user.name, c.user.id)))), 'on_connect': asyncio.coroutine(lambda: print('Connected!')), 'on_error': asyncio.coroutine(lambda ctx, error: c.__await(ctx.channel.send(error))), 'on_message': asyncio.coroutine((lambda m: ((c.__commands[m.content[2:]](m), c.on_message)[1] if m.content.startswith('!!') else c.on_message)))}.items()]), (lambda c: [print('Running...'), c.run(token)]))
